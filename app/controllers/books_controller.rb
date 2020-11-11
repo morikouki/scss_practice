9891dc78aba41a961fb2cded794ad192e1b9a52e
vagrant <akzouksia@icloud.com>
2020-11-11 13:00:39 +0900

    [Update]Book index_view

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,14 @@
 class BooksController < ApplicationController
 
 	def index
+		@book = Book.new
+		@books = Book.all
 	end
 
 	def create
+		book = Book.new(book_params)
+		book.save
+		redirect_to books_path
 	end
 
 	def show
